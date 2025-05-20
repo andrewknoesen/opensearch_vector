6860b65eb0cef4eb790959ee9b64deb8ec629d90
Andrew Knoesen <andrew@spatialedge.co.za>
2025-05-20 20:28:56 +0200

    Added the fine grained access = true to the app.py for opensearch

--- cdk/app.py
+++ cdk/app.py
@@ -36,7 +36,7 @@ vpc: VpcConfig = VpcConfig(
 )
 config: Config = (
     Config.new(PROJECT_NAME, vpc, tags)
-    .register_opensearch(OpensearchConfig(PROJECT_NAME, "t3.small.search")))
+    .register_opensearch(OpensearchConfig(PROJECT_NAME, "t3.small.search", fine_grained_access=True)))
 
 app = cdk.App()
 InfraStack(
@@ -44,16 +44,6 @@ InfraStack(
     "InfraStack",
     config=config,
     env=env,
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=cdk.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 )
 
 app.synth()
