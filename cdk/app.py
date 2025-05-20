8018cf226cc94524f9361ccc466110bec356cd69
Andrew Knoesen <andrew@spatialedge.co.za>
2025-05-20 20:25:40 +0200

    Test instance code complete and setup script complete

--- cdk/app.py
+++ cdk/app.py
@@ -1,28 +1,59 @@
 #!/usr/bin/env python3
 import os
 
+from typing import List
 import aws_cdk as cdk
 
 from infra.infra_stack import InfraStack
 
+from config.config import (
+    Config,
+    VpcConfig,
+    AwsAccount,
+    AwsAccountAlias,
+    AwsAccountNamespace,
+    SubnetConfig,
+    Region,
+    Tags,
+    OpensearchConfig,
+)
+
+PROJECT_NAME = "vector-store-demo"
+account = AwsAccount(AwsAccountNamespace.DEFAULT, "308388357296", AwsAccountAlias.DEV)
+
+env = cdk.Environment(account=account.number, region=Region.AF_SOUTH_1.value)
+
+tags: Tags = Tags(project_name=PROJECT_NAME)
+
+subnets: List[SubnetConfig]= [
+    SubnetConfig(id="subnet-0761dd00295d799f8", az="afs1-az1"),
+    SubnetConfig(id="subnet-0c707f058b5af7e8e", az="afs1-az2"),
+    SubnetConfig(id="subnet-04993ba77f8bc5bc9", az="afs1-az3")
+    ]
+
+vpc: VpcConfig = VpcConfig(
+    account=account, region=Region.AF_SOUTH_1, id="vpc-00a0b4c73f0b4d095", subnets=subnets
+)
+config: Config = (
+    Config.new(PROJECT_NAME, vpc, tags)
+    .register_opensearch(OpensearchConfig(PROJECT_NAME, "t3.small.search")))
 
 app = cdk.App()
-InfraStack(app, "InfraStack",
+InfraStack(
+    app,
+    "InfraStack",
+    config=config,
+    env=env,
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
-
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
+    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
+    # env=cdk.Environment(account='123456789012', region='us-east-1'),
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+)
 
 app.synth()
--- cdk/infra/infra_stack.py
+++ cdk/infra/infra_stack.py
@@ -1,19 +1,36 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    Environment,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
+from config.config import Config
 
+from infra.nested_stacks.opensearch_stack import OpensearchStack
 class InfraStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, construct_id: str, config: Config, env: Environment, **kwargs) -> None:
+        super().__init__(scope, construct_id, env=env, **kwargs)
+        vpc_config = config.vpc
+        vpc = ec2.Vpc.from_lookup(self, id="VPC", vpc_id=vpc_config.id)
 
-        # The code that defines your stack goes here
+        security_group: ec2.SecurityGroup = ec2.SecurityGroup(
+            self,
+            id="security-group",
+            vpc=vpc,
+            description=f"Security Group for {config.project_name}",
+            allow_all_outbound=True,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfraQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        for ingress_rule in [
+            "100.0.0.0/8",
+            "10.0.0.0/8",
+        ]:  # TODO: break this out into config.
+            security_group.add_ingress_rule(
+                peer=ec2.Peer.ipv4(ingress_rule), connection=ec2.Port.all_traffic()
+            )
+
+        opensearch: OpensearchStack = OpensearchStack(
+            self, "opensearch-stack", config=config, security_group=security_group, env=env
+        )
